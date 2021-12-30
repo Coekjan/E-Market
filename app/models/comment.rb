06c19cf2bc85116a00d711704da870a65d8bf495
coekjan <cn_yzr@qq.com>
2021-12-30 20:42:51 +0800

    修复评论自引用时删除引发死递归的 bug。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :account
   belongs_to :section
   belongs_to :comment
-  has_many :comments, dependent: :destroy
+  has_many :comments
   validates :content, presence: true
+
+  def destroy
+    comments.filter { |c| c != self }.each { |c| c.destroy }
+    super
+  end
 end
